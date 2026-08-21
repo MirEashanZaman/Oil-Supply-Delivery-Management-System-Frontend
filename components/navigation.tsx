59f6e42c65edf1dcf2448293ea4afd717c0496e2
EshanZaman <eshanzaman6699@gmail.com>
2026-08-22 01:30:01 +0600

    Fix navigation vertical stacking layout

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navigation() {
     return (
-        <>
+        <nav style={{ margin: "10px 0" }}>
             <Link href="/">Home</Link> |{" "}
             <Link href="/about">About Us</Link> |{" "}
             <Link href="/contact">Contact Us</Link> |{" "}
@@ -10,6 +10,6 @@ export default function Navigation() {
             <Link href="/products/2">Product 2</Link> |{" "}
             <Link href="/login">Login</Link> |{" "}
             <Link href="/register">Register</Link>
-        </>
+        </nav>
     );
 }

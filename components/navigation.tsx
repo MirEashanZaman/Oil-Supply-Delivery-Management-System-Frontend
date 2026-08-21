80e022fef43c19bbc3135747dc4c9af1f68e8159
EshanZaman <eshanzaman6699@gmail.com>
2026-08-22 01:26:02 +0600

    Add Login and Register to navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 export default function Navigation() {
     return (
         <>
-            <Link href="/">Home</Link>
-            <Link href="/about">About Us</Link>
-            <Link href="/contact">Contact Us</Link>
-            <Link href="/products/1">Product 1</Link>
-            <Link href="/products/2">Product 2</Link>
+            <Link href="/">Home</Link> |{" "}
+            <Link href="/about">About Us</Link> |{" "}
+            <Link href="/contact">Contact Us</Link> |{" "}
+            <Link href="/products/1">Product 1</Link> |{" "}
+            <Link href="/products/2">Product 2</Link> |{" "}
+            <Link href="/login">Login</Link> |{" "}
+            <Link href="/register">Register</Link>
         </>
     );
 }

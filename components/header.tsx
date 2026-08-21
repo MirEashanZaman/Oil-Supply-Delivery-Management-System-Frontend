a9b5af0d49e9a2403e0267ce19bcde3d79033d61
EshanZaman <eshanzaman6699@gmail.com>
2026-08-22 01:36:27 +0600

    Update logo src path to LOGO.png

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function MyHeader(props: { name: string; message: string }) {
     return (
         <>
-            <Image src="/images.jpeg" alt="Logo" width={150} height={150} />
+            <Image src="/LOGO.png" alt="Logo" width={150} height={150} />
             <h6>{props.name} Page</h6>
             <p>This is {props.message}</p>
         </>

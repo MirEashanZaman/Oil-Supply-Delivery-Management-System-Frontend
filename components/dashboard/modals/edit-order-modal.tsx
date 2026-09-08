64aaec369377ce4f99e91c2776714d849596f0f5
EshanZaman <eshanzaman6699@gmail.com>
2026-09-08 20:55:54 +0600

    Update components/dashboard/modals/edit-order-modal.tsx

--- components/dashboard/modals/edit-order-modal.tsx
+++ components/dashboard/modals/edit-order-modal.tsx
@@ -43,7 +43,7 @@ export const EditOrderModal: React.FC<EditOrderModalProps> = ({
           onClick={onClose}
           className="absolute top-4 right-4 text-slate-400 hover:text-white"
         >
-          ✕
+          
         </button>
         <h3 className="text-xl font-bold text-white mb-2">Edit Order Details</h3>
         <p className="text-xs text-amber-400 mb-4">

6af82da3c73b1fecefb116d44547e9c339a659da
EshanZaman <eshanzaman6699@gmail.com>
2026-09-16 20:29:26 +0600

    Add Capacitor app configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Eshan.oilsupply',
+  appName: 'Oil Supply Delivery',
+  webDir: 'public'
+};
+
+export default config;
